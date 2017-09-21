f4673182ee7529b7ddd050f83c1381c508a10a7c
GraciousVertex32 <31588270+GraciousVertex32@users.noreply.github.com>
2017-09-22 00:38:19 +0800

    Core Structure Changes
    repalce multiple string with array and loop for same part for multi number protencial
    
    known bugs:unable to reset after first caculate

--- Caculator01/Form1.cs
+++ Caculator01/Form1.cs
@@ -13,11 +13,10 @@ namespace Caculator01
 {
     public partial class Form1 : Form
     {
-        public static string tempstr1 = null;
-        public static string tempstr2 = null;
+        public string[] tempstr = new string[10];
         public char method1 = 'n';
         public char method2 = 'n';
-        
+        public int stringindex = 0;
         public double result = new double();
         public Form1()
         {
@@ -26,8 +25,8 @@ namespace Caculator01
 
         public double Basicfunction()
         {
-            double temp1 = double.Parse(tempstr1);
-            double temp2 = double.Parse(tempstr2);
+            double temp1 = double.Parse(tempstr[0]);
+            double temp2 = double.Parse(tempstr[1]);
             switch (method1)
             {
                 case '+':
@@ -53,9 +52,65 @@ namespace Caculator01
         }
         //计算部分
 
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            stringindex = 0;
+            string str = textBox1.Text;
+            int breakpoint=0;
+            while(breakpoint<str.Length)
+            {
+                for (int i = breakpoint; i < str.Length; i++)
+                {
+                    if (str[i] <= '9' && str[i] >= '0')
+                    {
+                        tempstr[stringindex] = tempstr[stringindex]+ str[i];
+                        breakpoint = i+1;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                stringindex++;
+                //提取第一组数字
+                label2.Text = tempstr[stringindex-1];
+               
+                if (breakpoint < str.Length)
+                {
+                    char chr = new char();
+                    chr = str[breakpoint];
+                    switch (chr)
+                    {
+                        case '+':
+                            method1 = '+';
+                            break;
+                        case '-':
+                            method1 = '-';
+                            break;
+                        case '*':
+                            method1 = '*';
+                            break;
+                        case '/':
+                            method1 = '/';
+                            break;
+                        case '^':
+                            method1 = '^';
+                            break;
+                        default:
+                            Console.WriteLine("Invalid Expression");
+                            break;
+                    }
+                }
+                breakpoint++;
+            }
+
+            label1.Text = Basicfunction().ToString();
+            //调用计算函数，输出
+        }
         private void button3_Click(object sender, EventArgs e)
         {
-            string path = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase; 
+            string path = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
             FileStream fs = new FileStream(path, FileMode.Create);
             StreamWriter sw = new StreamWriter(fs);
             sw.Write(result);
@@ -64,62 +119,5 @@ namespace Caculator01
             fs.Close();
         }
         //保存结果
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            string str = textBox1.Text;
-            int breakpoint=0;
-            for(int i= 0; i<str.Length; i++)
-            {
-                if(str[i]<='9' && str[i]>='0')
-                {
-                    tempstr1 = tempstr1 + str[i];
-                }
-                else
-                {
-                    breakpoint = i;
-                    break;
-                }
-            }
-            label2.Text = tempstr1;
-            //提取第一组数字
-            switch(str[breakpoint])
-            {
-                case '+':
-                    method1 = '+';
-                    break;
-                case '-':
-                    method1 = '-';
-                    break;
-                case '*':
-                    method1 = '*';
-                    break;
-                case '/':
-                    method1 = '/';
-                    break;
-                case '^':
-                    method1 = '^';
-                    break;
-                default:
-                    Console.WriteLine("Invalid Expression");
-                    break;
-            }
-            //提取第一个运算符
-            for (int i = breakpoint+1; i < str.Length; i++)
-            {
-                if (str[i] <= '9' && str[i] >= '0')
-                {
-                    tempstr2 = tempstr2 + str[i];
-                }
-                else
-                {
-                    breakpoint = i;
-                    break;
-                }
-            }
-            label3.Text = tempstr2;
-            //提取第二组数字
-            label1.Text = Basicfunction().ToString();
-        }
     }
 }
