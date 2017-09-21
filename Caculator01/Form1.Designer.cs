34f9584e701c1161c0832ee020ec1e8024ae683d
haobo lang <1977610912@qq.com>
2017-09-21 12:12:41 +0800

    unload
    file

---
+++ Caculator01/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Caculator01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(190, 188);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(61, 37);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(42, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(42, 189);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(76, 35);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "caculate";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(192, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(192, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Caculator01/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Caculator01
+{
+    public partial class Form1 : Form
+    {
+        public static string tempstr1 = null;
+        public static string tempstr2 = null;
+        public char method1 = 'n';
+        public char method2 = 'n';
+        
+        public double result = new double();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public double Basicfunction()
+        {
+            double temp1 = double.Parse(tempstr1);
+            double temp2 = double.Parse(tempstr2);
+            switch (method1)
+            {
+                case '+':
+                    return temp1 + temp2;
+                    break;
+                case '-':
+                    return temp1 - temp2;
+                    break;
+                case '*':
+                    return temp1 * temp2;
+                    break;
+                case '/':
+                    return temp1 / temp2;
+                    break;
+                case '^':
+                    return Math.Pow(temp1,temp2);
+                    break;
+                default:
+                    Console.WriteLine("Must be Seriously Wrong");
+                    return 55555555555;
+                    break;
+            }
+        }
+        //计算部分
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string path = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase; 
+            FileStream fs = new FileStream(path, FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.Write(result);
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+        }
+        //保存结果
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string str = textBox1.Text;
+            int breakpoint=0;
+            for(int i= 0; i<=str.Length; i++)
+            {
+                if(str[i]<=9 && str[i]>=0)
+                {
+                    tempstr1 = tempstr1 + str[i];
+                }
+                else
+                {
+                    breakpoint = i;
+                    break;
+                }
+            }
+            label2.Text = tempstr1;
+            //提取第一组数字
+            switch(str[breakpoint])
+            {
+                case '+':
+                    method1 = '+';
+                    break;
+                case '-':
+                    method1 = '-';
+                    break;
+                case '*':
+                    method1 = '*';
+                    break;
+                case '/':
+                    method1 = '/';
+                    break;
+                case '^':
+                    method1 = '^';
+                    break;
+                default:
+                    Console.WriteLine("Invalid Expression");
+                    break;
+            }
+            //提取第一个运算符
+            for (int i = breakpoint+1; i <= str.Length; i++)
+            {
+                if (str[i] <= 9 && str[i] >= 0)
+                {
+                    tempstr2 = tempstr2 + str[i];
+                }
+                else
+                {
+                    breakpoint = i;
+                    break;
+                }
+            }
+            label3.Text = tempstr2;
+            //提取第二组数字
+            
+        }
+    }
+}
